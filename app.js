437d793da947947c5cdbc36c0448d0822c62e4d4
Ghiwook Nam <occhio@vinylc.com>
2017-03-13 13:48:03 +0900

    ..

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var EventEmitter = require('events')
 var restify = require('restify')
+var http = require("http");
 
 var server = restify.createServer();
 
@@ -16,14 +17,43 @@ server.get("/keyboard", function (req, res, next) {
 });
 
 server.post("/message", function (req, res, next) {
-    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-    var obj = {};
-    obj.message = {};
-    obj.message.text = "hello";
-    obj.keyboard = {};
-    obj.keyboard.type = "text";
-    res.end(JSON.stringify(obj));
-    return next();
+
+    if (req.body == "vinylc") {
+        var options = {
+            hostname: 'thawing-sierra-80063.herokuapp.com',
+            path: '/message',
+            method: 'POST'
+        };
+        http.request(options, function (response) {
+            var data = '';
+            response.on('data', function (chunk) {
+                data += chunk;
+            });
+            response.on('end', function () {
+                console.log(data);
+
+                res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+                var obj = {};
+                obj.message = {};
+                obj.message.text = data;
+                obj.keyboard = {};
+                obj.keyboard.type = "text";
+                res.end(JSON.stringify(obj));
+                return next();
+
+            });
+        }).end();
+    } else {
+        res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+        var obj = {};
+        obj.message = {};
+        obj.message.text = "hello";
+        obj.keyboard = {};
+        obj.keyboard.type = "text";
+        res.end(JSON.stringify(obj));
+        return next();
+    }
+
 });
 
 server.post("/friend", function (req, res, next) {
